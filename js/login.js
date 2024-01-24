227c0010d080d12fb0210358523db26e6d61c953
MauricioPérez <69589423+CxAx@users.noreply.github.com>
2024-01-24 23:27:31 +0000

    Validacion JS

--- js/login.js
+++ js/login.js
@@ -1,7 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("Hello, World!");
-    var inputEmail = document.getElementById('usuario')
-    var inputPassword = document.getElementById('password')
-    console.log(inputEmail)
-    console.log(inputPassword)
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('.login-form');
+    var usuarioInput = document.getElementById('Usuario');
+    var passwordInput = document.getElementById('password');
+
+    form.addEventListener('submit', function (event) {
+        
+        var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(usuarioInput.value)) {
+            alert('El usuario no tiene un formato de correo electrónico válido.');
+            event.preventDefault(); // Evitar el envío del formulario
+            return;
+        }
+
+        if (passwordInput.value.length >= 8) {
+            alert('La contraseña debe tener menos de 8 caracteres.');
+            event.preventDefault(); // Evitar el envío del formulario
+            return;
+        }
+
+    });
 });
