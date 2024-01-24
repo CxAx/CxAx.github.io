27ee94eb4440232e19183a3bb2d2a5042cd1a570
MauricioPérez <69589423+CxAx@users.noreply.github.com>
2024-01-24 23:12:53 +0000

    Se agrega informacion a los archivos

---
+++ js/login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Hello, World!");
+    var inputEmail = document.getElementById('usuario')
+    var inputPassword = document.getElementById('password')
+    console.log(inputEmail)
+    console.log(inputPassword)
+});
